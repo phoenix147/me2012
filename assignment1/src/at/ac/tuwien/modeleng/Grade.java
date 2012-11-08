416ffcc3187b658a34bfaa0351e035093dc1e70f
Alexander Eibner <ax@flynn.thek.lan>
2012-11-08 22:51:07 +0100

    Init ass1 A1

---
+++ assignment1/src/at/ac/tuwien/modeleng/Grade.java
@@ -0,0 +1,25 @@
+package at.ac.tuwien.modeleng;
+
+public class Grade {
+	
+	private String name;
+	
+	private int requiredPoints;
+ 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRequiredPoints() {
+		return requiredPoints;
+	}
+
+	public void setRequiredPoints(int requiredPoints) {
+		this.requiredPoints = requiredPoints;
+	}
+
+}
