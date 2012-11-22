fc5185b31e288c8383087478d9cc63a4f950aec7
Alexander Eibner <ax@flynn.thek.lan>
2012-11-22 18:03:53 +0100

    Fixed project setup for xtext, run tests and extracted xmis

--- at.ac.tuwien.big.me12.GSMLDsl/src-gen/at/ac/tuwien/big/me12/AbstractGSMLDslRuntimeModule.java
+++ at.ac.tuwien.big.me12.GSMLDsl/src-gen/at/ac/tuwien/big/me12/AbstractGSMLDslRuntimeModule.java
@@ -31,7 +31,7 @@ public abstract class AbstractGSMLDslRuntimeModule extends DefaultRuntimeModule 
 	
 	public void configureFileExtensions(Binder binder) {
 		if (properties == null || properties.getProperty(Constants.FILE_EXTENSIONS) == null)
-			binder.bind(String.class).annotatedWith(Names.named(Constants.FILE_EXTENSIONS)).toInstance("mydsl");
+			binder.bind(String.class).annotatedWith(Names.named(Constants.FILE_EXTENSIONS)).toInstance("gsml");
 	}
 	
 	// contributed by org.eclipse.xtext.generator.grammarAccess.GrammarAccessFragment
--- at.ac.tuwien.big.me12.GSMLDsl/src-gen/at/ac/tuwien/big/me12/GSMLDslStandaloneSetupGenerated.java
+++ at.ac.tuwien.big.me12.GSMLDsl/src-gen/at/ac/tuwien/big/me12/GSMLDslStandaloneSetupGenerated.java
@@ -30,8 +30,8 @@ public class GSMLDslStandaloneSetupGenerated implements ISetup {
 
 		org.eclipse.xtext.resource.IResourceFactory resourceFactory = injector.getInstance(org.eclipse.xtext.resource.IResourceFactory.class);
 		org.eclipse.xtext.resource.IResourceServiceProvider serviceProvider = injector.getInstance(org.eclipse.xtext.resource.IResourceServiceProvider.class);
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("mydsl", resourceFactory);
-		org.eclipse.xtext.resource.IResourceServiceProvider.Registry.INSTANCE.getExtensionToFactoryMap().put("mydsl", serviceProvider);
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("gsml", resourceFactory);
+		org.eclipse.xtext.resource.IResourceServiceProvider.Registry.INSTANCE.getExtensionToFactoryMap().put("gsml", serviceProvider);
 		
 
 
